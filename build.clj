5917e44f663a0141c27d57573166ae72f4189897
Laurens Van Houtven (lvh) <lvh@latacora.com>
2021-10-13 21:58:55 -0500

    Start of aws api releases I guess

--- build.clj
+++ build.clj
@@ -2,13 +2,22 @@
   (:refer-clojure :exclude [test])
   (:require
     [clojure.tools.build.api :as b] ; for b/git-count-revs
-    [org.corfield.build :as bb]))
+    [org.corfield.build :as bb]
+    [clojure.edn :as edn]))
 
 (def lib 'com.latacora/backsaws)
 (def version (format "1.0.%s" (b/git-count-revs nil)))
 
 (defn test "Run the tests." [opts]
   (bb/run-tests opts))
+
+(def ^:private aws-api-releases-file
+  "https://raw.githubusercontent.com/cognitect-labs/aws-api/master/latest-releases.edn")
+
+(-> aws-api-releases-file
+    slurp
+    edn/read-string
+    )
 
 (defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
   (-> opts
